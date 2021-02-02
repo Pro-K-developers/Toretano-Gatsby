b9c830dd29f866cb9befeda5da392ca0e404d4d0
yuzusuzuki <okashidaisukiyzyz@gmail.com>
2021-02-02 22:15:28 +0900

    first commit

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,24 @@
 import React from "react"
 import contactStyles from "./contact.module.css"
+import Title from "../Title"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Contact = () => {
-  return <div className={contactStyles.container}>Contact</div>
+  return (
+    <div className={contactStyles.container}>
+      <Title title="Contact" subtitle="もっと知りたい方はこちらも" />
+      <Img src="http://placehold.jp/{100}x{100}.png" />
+      <ul className={contactStyles.a}>
+        <li>Facebook</li>
+        <li>Instagram</li>
+      </ul>
+      <ul className={contactStyles.b}>
+        <li>Facebook</li>
+        <li>Instagram</li>
+      </ul>
+    </div>
+  )
 }
 
 export default Contact
--- src/components/Feature/Feature.jsx
+++ src/components/Feature/Feature.jsx
@@ -1,8 +1,23 @@
 import React from "react"
 import featureStyles from "./feature.module.css"
+import Title from "../Title"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
 
 const Feature = () => {
-  return <div className={featureStyles.container}>Feature</div>
+  return (
+    <div className={featureStyles.container}>
+      <Title title="Feature" subtitle="イチオシ" />
+      <p className={featureStyles.honey_pc}>
+        現在、とれたのでは中央蜂蜜さまのはちみつを販売中！<br></br>
+        国産も含め、全て無添加の純粋はちみつとなっております。
+      </p>
+      <p className={featureStyles.honey_mobile}>
+        現在、とれたのでは中央蜂蜜さまの<br></br>はちみつを販売中！<br></br>
+        国産も含め、全て無添加の<br></br>純粋はちみつとなっております。
+      </p>
+    </div>
+  )
 }
 
 export default Feature
