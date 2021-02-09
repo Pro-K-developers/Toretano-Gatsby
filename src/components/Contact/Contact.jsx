6026a60c5d0ac06cffcf30cab49ea2f96655fa16
Yuzu <okashidaisukiyzyz@gmail.com>
2021-02-09 12:30:49 +0900

    facebook

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -10,11 +10,66 @@ const Contact = () => {
       <Title title="Contact" subtitle="もっと知りたい方はこちらも" />
 
       <ul className={contactStyles.a}>
-        <li>Facebook</li>
+        <li>
+          Facabook
+          <div id="fb-root"></div>
+          <script
+            async
+            defer
+            crossorigin="anonymous"
+            src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v9.0"
+            nonce="X31xHV0s"
+          ></script>
+          <div
+            class="fb-page"
+            data-href="https://www.facebook.com/toretano/"
+            data-tabs="timeline"
+            data-width="337"
+            data-height="388"
+            data-small-header="false"
+            data-adapt-container-width="true"
+            data-hide-cover="false"
+            data-show-facepile="true"
+          >
+            <blockquote
+              cite="https://www.facebook.com/toretano/"
+              class="fb-xfbml-parse-ignore"
+            >
+              <a href="https://www.facebook.com/toretano/">とれたの</a>
+            </blockquote>
+          </div>
+        </li>
         <li>Instagram</li>
       </ul>
       <ul className={contactStyles.b}>
-        <li>Facebook</li>
+        <li>
+          Facebook<div id="fb-root"></div>
+          <script
+            async
+            defer
+            crossorigin="anonymous"
+            src="https://connect.facebook.net/ja_JP/sdk.js#xfbml=1&version=v9.0"
+            nonce="X31xHV0s"
+          ></script>
+          <div
+            class="fb-page"
+            data-href="https://www.facebook.com/toretano/"
+            data-tabs="timeline"
+            data-width="337"
+            data-height="388"
+            data-small-header="false"
+            data-adapt-container-width="true"
+            data-hide-cover="false"
+            data-show-facepile="true"
+          >
+            <blockquote
+              cite="https://www.facebook.com/toretano/"
+              class="fb-xfbml-parse-ignore"
+            >
+              <a href="https://www.facebook.com/toretano/">とれたの</a>
+            </blockquote>
+          </div>
+        </li>
         <li>Instagram</li>
       </ul>
     </div>
